104f02533611583d003af39ec8ffa46395856cfe
1JuL <diegodiax0122@gmail.com>
2025-04-14 01:49:02 -0500

    Removed Async Storage, for better session handling

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,8 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { initializeAuth } from "firebase/auth";
-import { getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -14,7 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
+export const auth = initializeAuth(app);
 export const db = getFirestore(app);
